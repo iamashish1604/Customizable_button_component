f979feb9001538aecf61870db84eccffb8216a3c
ASHISH SINGH <as676967@gmail.com>
2021-07-11 12:01:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h2>Problem Statement : Create a customizable button component.</h2>
       <h2>
         Solution : Below are the customizable button components,which can be
-        customized based on size and color.{" "}
+        customized based on size and color.
       </h2>
       <Btn type="primary" size="small" text="Blue" />
       <Btn type="secondary" size="medium" text="Green" />
